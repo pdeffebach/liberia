f5509e2b926c3b690ba44bb3c304e48be4e03919
pdeffebach <p.deffebach@gmail.com>
2019-01-11 10:08:34 -0500

    Add comments for power calculations

--- do/power_calcs.r
+++ do/power_calcs.r
@@ -1,10 +1,21 @@
 # power calculations
+
+################################################################################
+# Add relevant packages ########################################################
+################################################################################
+# todo: figure out how to use packrat and projects to automate this
 library(tidyverse)
 library(haven)
 library(ICC)
 library(knitr)
 library(kableExtra)
+library(clusterPower)
 
+################################################################################
+# Import the data set ##########################################################
+################################################################################
+# This dataset is just a small subset of variables we need, and just the 
+# 4,000 individual level observations for the second round.
 df <- haven::read_dta("data/power_calcs.dta")
 vars_to_power <- c(
     "anylndconf_u_ec2",
@@ -14,7 +25,11 @@ vars_to_power <- c(
     "lmg_unrslv_conf_u_ec2",
     "lmg_conf_any_u_ec2") 
 
-# hard coded for now, so dont change the list above!
+################################################################################
+# Control means, treatments effects, and treatment SEs for each variable #######
+################################################################################
+# hard coded for now, so dont change the list of variables above!
+# todo: figure out why I was dumb and did this. Fix. 
 control_means <- c(
     0.087,
     0.024,
@@ -41,6 +56,12 @@ treatment_ses <- c(
     0.009,
     0.010) 
 
+################################################################################
+# Define a function for getting the labels as a list ###########################
+################################################################################
+# Remember that the `haven` package saves the labels of stata variables in 
+# an attribute called "label". This function just gets those out and puts
+# them in a nice tibble. 
 makeVlist <- function(dta) { 
      labels <- sapply(dta, function(x) attr(x, "label"))
       tibble(variable = names(labels),
@@ -48,16 +69,27 @@ makeVlist <- function(dta) {
 }
 labels = makeVlist(df[,vars_to_power])
 
+################################################################################
+# Find the size of each effect as a t-stat #####################################
+################################################################################
+# Initiate a vector of all effect sizes, which we will append to
 effect_sizes = vector()
+
 for(var in vars_to_power) {
     # get the intra-class correlation
     icc = ICCbare(df$commcode, df[[var]])
+
+    # get the within-group variance
+    # todo: document where I got this. I think it was from googling how to 
     # get the within-group variance
     model = anova(lm(df[[var]] ~ factor(df[["commcode"]])))
     varw = model[["Mean Sq"]][1]
+
+    # Look up the documentation for clusterPower to understand what's going
+    # on here. 
     effect_size = crtpwr.2mean(
         alpha = .05, 
-        power = .08, 
+        power = .8, 
         m = 102, 
         n = 20, 
         cv = 0, 
@@ -66,6 +98,8 @@ for(var in vars_to_power) {
         varw = varw)
     effect_sizes <- c(effect_sizes, effect_size)
 }
+
+# Put everything into a tibble that is combined with the control mean etc.
 powers = tibble(
     variable = vars_to_power, 
     mde = effect_sizes, 
@@ -73,14 +107,16 @@ powers = tibble(
     control_mean = control_means,
     treatment_se = treatment_ses)
 
+# Add in the labels that we got from the makeVlist function above
 powers <- powers %>%     
     inner_join(labels, by = "variable") %>% 
     mutate(standardized_treatment_effect = treatment_effect / treatment_se) %>%
     mutate(effect_as_pct_mde = abs(standardized_treatment_effect) / mde) 
 
-
+# Combine everything together, for real this time.
 out <- powers %>% select(label, standardized_treatment_effect, mde, effect_as_pct_mde)
 
+# Use kable, a nice way to format latex tables, for output.
 table = kable(
     out, 
     format = "html",
